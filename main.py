b1e44a6c0fafd4dc8f6a11d00b40b2fde911b7f0
LauZhanYou <119853913+donkey-king-kong@users.noreply.github.com>
2024-10-04 18:21:18 +0800

    Update main with DB functions

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from document_specific_chunk import markdown_chunk_text
 from createEmbeddings import getResponse
+from connectDB import queryDB, insertDB, createTable, dropTable
 
 folder_path = "resumes"
 resumeChunks = markdown_chunk_text(folder_path)
 
+# Chunking / Embedding / Storing
 for filename, chunks in resumeChunks.items():
     print(f"Chunks for {filename}:")
     for i, chunk in enumerate(chunks):
-        print(f"Chunk {i+1}")
-        print(getResponse(chunk.page_content))
-        print()
+        # Embedding each chunk
+        vectorEmbedding = getResponse(chunk.page_content)
+        # Printing the chunk number
+        print(f"Chunk {i+1}: ")
+        # Store each embedding into the database
+        insertDB(chunk.page_content, vectorEmbedding)
+
+# Query
+# Embed the query
+query = input("Enter query: ")
+queryEmbedding = getResponse(query)
+# Query the database
+queryDB(queryEmbedding)
